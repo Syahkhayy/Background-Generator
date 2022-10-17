9cd6c467bb0488b38afaf4a3b985100397e8f5bb
SyahmiKhairul <syahmi2001@gmail.com>
2022-10-17 11:34:57 +0800

    Add onload Event On <body> Section

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
-function setGradient(){
-    body.style.background =
+function setGradient() {
+  body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ";";
 }
